9b9a3a7b95e1ad71bd582a1efa037d48fe00782a
Gurvan <gurvancambray@gmail.com>
2023-09-18 15:33:56 +0200

    :lipstick:Introduction de tailwind css:lipstick:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,18 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
 
         <title>Laravel</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-        @vite('../ressources/css/app.css')
+        <!-- Style -->
     </head>
     <body >
-        <h1 class="text-3xl font-bold underline">Hello les potes à la compote</h1>
+    <h1 class="text-8xl font-bold underline">
+        Hello world!
+    </h1>
     </body>
 </html>
