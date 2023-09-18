ac26af7ed288b3f110d0938449593cb4a1294465
Gurvan <gurvancambray@gmail.com>
2023-09-18 14:25:08 +0200

    :lipstick:Introduction de tailwind css:lipstick:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-
+    "./ressources/**/*.blade.php",
+      "./ressources/**/*.js",
+      "./ressources/**/*.vue"
   ],
   theme: {
     extend: {},
